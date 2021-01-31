b40e00d969f3c773564bbb6ebc637617257621ae
yarastqt <yarastqt@gmail.com>
2021-02-01 00:56:21 +0300

    feat: setup swagger for app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  // prettier-ignore
-  (await NestFactory.create(AppModule))
-    .setGlobalPrefix('/api/v1')
-    .listen(3000)
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Notebox API')
+    .setVersion('v1')
+    .setBasePath('/api/v1')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('/api/v1', app, document)
+  app.setGlobalPrefix('/api/v1').listen(3000)
 }
+
 bootstrap()
