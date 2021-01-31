6d0823357d1963223a985742019fd7733c6e4a82
yarastqt <yarastqt@gmail.com>
2021-02-01 00:57:11 +0300

    feat: add stub for notes feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './features/auth/AuthModule'
+import { NotesModule } from './notes/notes.module'
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, NotesModule],
 })
 export class AppModule {}
