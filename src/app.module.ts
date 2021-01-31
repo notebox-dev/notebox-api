0bbaab4080cce8ae20c79ab0fe049fa976ad5e2d
yarastqt <yarastqt@gmail.com>
2021-02-01 01:44:18 +0300

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './features/auth/AuthModule'
 import { NotesModule } from './notes/notes.module'
 
 @Module({
-  imports: [AuthModule, NotesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }),
+    AuthModule,
+    NotesModule,
+  ],
 })
 export class AppModule {}
