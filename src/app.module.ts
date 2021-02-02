16c57a60888d3185b11c51e17278a4180132ec41
yarastqt <yarastqt@gmail.com>
2021-02-03 00:03:52 +0300

    feat: add custom configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './features/auth/AuthModule'
 import { NotesModule } from './notes/notes.module'
+import { configure } from './configs'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.development.env',
+      load: [configure],
+      envFilePath: ['.development.env'],
     }),
     AuthModule,
     NotesModule,
