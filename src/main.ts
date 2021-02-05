192eaf6c1804ee4552dd48bacaa0425cc6aba122
yarastqt <yarastqt@gmail.com>
2021-02-06 00:10:09 +0300

    feat(main): use ValidationPipe as global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -15,9 +17,12 @@ async function bootstrap() {
 
   SwaggerModule.setup('/api/v1', app, document)
 
+  // TODO: Add mapping for errors.
+  app.useGlobalPipes(new ValidationPipe())
   app.disable('x-powered-by')
   app.setGlobalPrefix('/api/v1')
-  app.listen(3000)
+
+  await app.listen(3000)
 }
 
 bootstrap()
