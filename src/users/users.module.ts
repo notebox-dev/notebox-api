e862f20865fc9dfdf40b9d88ccc0970db37bb911
yarastqt <yarastqt@gmail.com>
2021-02-13 20:34:18 +0300

    refactor(users): import EncryptionModule from root

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
-import { EncryptionModule } from 'src/lib/encryption/encryption.module'
+import { EncryptionModule } from 'src/lib/encryption'
 import { UsersService } from './users.service'
 import { User } from './entities/user.entity'
 
